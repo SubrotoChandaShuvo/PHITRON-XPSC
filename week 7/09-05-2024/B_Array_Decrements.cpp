80f053594f5d77db8778df00120b2bd905929b0c
SubrotoChandaShuvo <140307494+SubrotoChandaShuvo@users.noreply.github.com>
2024-05-10 01:57:06 +0600

    Update B_Array_Decrements.cpp

--- week 7/09-05-2024/B_Array_Decrements.cpp
+++ week 7/09-05-2024/B_Array_Decrements.cpp
@@ -5,51 +5,60 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     int t;
-    cin>>t;
+    cin >> t;
     while(t--)
     {
         int n;
-        cin>>n;
-        vector<int> a(n),b(n),v;
-        for(int i=0; i<n; i++)
-        cin>>a[i];
-        for(int i=0; i<n; i++)
-        cin>>b[i];
-        bool ans=true;
-        int dif,difz=INT_MIN;
-        
-        for(int i=0; i<n; i++)
+        cin >> n;
+
+        vector<int> a(n);
+        vector<int> b(n);
+        vector<int> v;
+
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        for (int i = 0; i < n; i++)
+            cin >> b[i];
+
+        bool ans = true;
+        int dif, difz = INT_MIN;
+
+        for (int i = 0; i < n; i++)
         {
-            if(a[i]<b[i] )
+            if (a[i] < b[i])
             {
-                ans=false;
+                ans = false;
                 break;
             }
-
-            if(b[i]==0)
-            difz=max(difz,a[i]);
+            else if (b[i] == 0)
+            {
+                difz = max(difz, a[i]);
+            }
             else
             {
-                
-                dif=a[i]-b[i];
-                if(i>0)
+                dif = a[i] - b[i];
+                if (!v.empty())
                 {
-                    int pre=v.back();
+                    int pre = v.back();
                     v.push_back(dif);
-                    if(pre!=v.back())
+                    if (pre != v.back())
                     {
-                        ans=false;
+                        ans = false;
                         break;
                     }
                 }
                 else
-                v.push_back(dif);
-
+                    v.push_back(dif);
             }
-
         }
-        if(ans and v.back()>difz)     cout<<"YES\n";
-        else        cout<<"NO\n";
+        if(!v.empty() && (v.back() < difz))
+        ans=false;
+        
+        if (ans )
+            cout << "YES\n";
+        else
+            cout << "NO\n";
     }
     return 0;
 }
